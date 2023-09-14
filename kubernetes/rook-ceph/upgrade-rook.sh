0389a97659336493b5851799c175ef998689df79
Alexander Städing <alexanderstaeding@gmail.com>
2023-09-14 11:18:00 +0200

    Fix upgrade-rook script to also update operator image

--- kubernetes/rook-ceph/upgrade-rook.sh
+++ kubernetes/rook-ceph/upgrade-rook.sh
@@ -18,23 +18,25 @@ is_tag() {
   wget -qO- https://api.github.com/repos/rook/rook/git/refs/tags/$1 &> /dev/null
 }
 
-# Read the currently installed version from rook-version.txt, if available
+# Read the currently installed version
 if [[ -f "rook-version.txt" ]]; then
   CURRENT_VERSION=$(<rook-version.txt)
   echo "Current version from rook-version.txt: ${CURRENT_VERSION}"
 else
-  echo "No rook-version.txt file found. Unable to determine current version."
+  echo "No rook-version.txt found. Unable to determine current version."
 fi
 
 # Validate input
 if [[ "$#" -eq 0 ]]; then
-    echo "No argument provided. Aborting."
-    echo "Usage: ./upgrade-rook.sh [stable | <version>]"
-    exit 1
+  echo "No argument provided. Aborting."
+  echo "Usage: ./upgrade-rook.sh [stable | <version>]"
+  exit 1
 elif [[ "$#" -ne 1 ]]; then
-    echo "Invalid number of arguments. Usage: ./upgrade-rook.sh [stable | <version>]"
-    exit 1
+  echo "Invalid number of arguments. Usage: ./upgrade-rook.sh [stable | <version>]"
+  exit 1
 fi
+
+# Fetch and validate the new version
 if [[ "$1" == "stable" ]]; then
   VERSION=$(get_latest_version)
   read -p "The latest stable version is ${VERSION}. Would you like to proceed? (y/n): " proceed
@@ -56,24 +58,25 @@ else
   fi
 fi
 
-# Define URLs
+# Update the operator.yaml file
+sed -i "s|image: rook/ceph:.*|image: rook/ceph:${VERSION}|" operator.yaml
+
+# Fetch the new YAML files
 COMMON_URL="https://raw.githubusercontent.com/rook/rook/${VERSION}/deploy/examples/common.yaml"
 CRDS_URL="https://raw.githubusercontent.com/rook/rook/${VERSION}/deploy/examples/crds.yaml"
-
-# Fetch files using wget
 wget -O common.yaml $COMMON_URL
 wget -O crds.yaml $CRDS_URL
 
-# Update rook-version.txt with the new version
+# Update rook-version.txt
 echo $VERSION > rook-version.txt
 
 # Suggest to create a Git commit
-echo "Files common.yaml and crds.yaml have been updated to version ${VERSION}."
+echo "Files common.yaml, crds.yaml, and operator.yaml have been updated to version ${VERSION}."
 echo "rook-version.txt has been updated to version ${VERSION}."
 read -p "Would you like to create a Git commit for these changes? (y/n): " choice
 
 case "$choice" in
-  y|Y ) git add common.yaml crds.yaml rook-version.txt
+  y|Y ) git add common.yaml crds.yaml operator.yaml rook-version.txt
         git commit -m "Upgrade Rook-Ceph to version ${VERSION}"
         ;;
   n|N ) echo "Skipping Git commit. You can manually commit the changes later."
