8658685fb1b74bc787b59d544c4bbc7907bb1ee0
Alexander Städing <alexanderstaeding@gmail.com>
2023-09-14 11:25:13 +0200

    Update upgrade-rook script to check current version and optimize wget output

--- kubernetes/rook-ceph/upgrade-rook.sh
+++ kubernetes/rook-ceph/upgrade-rook.sh
@@ -26,46 +26,63 @@ else
   echo "No rook-version.txt found. Unable to determine current version."
 fi
 
-# Validate input
-if [[ "$#" -eq 0 ]]; then
-  echo "No argument provided. Aborting."
-  echo "Usage: ./upgrade-rook.sh [stable | <version>]"
-  exit 1
-elif [[ "$#" -ne 1 ]]; then
-  echo "Invalid number of arguments. Usage: ./upgrade-rook.sh [stable | <version>]"
+# Initialize variables
+VERSION=""
+FORCE=0
+
+# Parse input arguments
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --force)
+      FORCE=1
+      ;;
+    stable)
+      VERSION=$(get_latest_version)
+      ;;
+    *)
+      VERSION="$1"
+      ;;
+  esac
+  shift
+done
+
+# Validation
+if [[ -z "$VERSION" ]]; then
+  echo "No version specified. Aborting."
   exit 1
 fi
 
-# Fetch and validate the new version
-if [[ "$1" == "stable" ]]; then
-  VERSION=$(get_latest_version)
-  read -p "The latest stable version is ${VERSION}. Would you like to proceed? (y/n): " proceed
-  if [[ "$proceed" != "y" && "$proceed" != "Y" ]]; then
+# Check for the same version and force flag
+if [[ "$VERSION" == "$CURRENT_VERSION" ]]; then
+  if [[ "$FORCE" -eq 0 ]]; then
+    echo "Version ${VERSION} is already installed. Use --force to override. Aborting."
+    exit 1
+  else
+    echo "Forcing upgrade to the same version ${VERSION}."
+  fi
+fi
+
+# Further validation on the ref
+if ! ref_exists $VERSION; then
+  echo "Error: The ref ${VERSION} does not exist."
+  exit 1
+elif ! is_tag $VERSION; then
+  read -p "Warning: The ref ${VERSION} is not a tag. Proceed anyway? (y/n): " confirm
+  if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
     echo "Aborting."
     exit 1
   fi
-else
-  VERSION=$1
-  if ! ref_exists $VERSION; then
-    echo "Error: The ref ${VERSION} does not exist."
-    exit 1
-  elif ! is_tag $VERSION; then
-    read -p "Warning: The ref ${VERSION} is not a tag. Proceed anyway? (y/n): " confirm
-    if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
-      echo "Aborting."
-      exit 1
-    fi
-  fi
 fi
 
-# Update the operator.yaml file
+# Update operator.yaml file
 sed -i "s|image: rook/ceph:.*|image: rook/ceph:${VERSION}|" operator.yaml
 
 # Fetch the new YAML files
 COMMON_URL="https://raw.githubusercontent.com/rook/rook/${VERSION}/deploy/examples/common.yaml"
 CRDS_URL="https://raw.githubusercontent.com/rook/rook/${VERSION}/deploy/examples/crds.yaml"
-wget -O common.yaml $COMMON_URL
-wget -O crds.yaml $CRDS_URL
+
+wget -q --show-progress -O common.yaml $COMMON_URL
+wget -q --show-progress -O crds.yaml $CRDS_URL
 
 # Update rook-version.txt
 echo $VERSION > rook-version.txt
